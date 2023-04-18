06e748d69d6681e9f1b45c9870d9dba24e5579bd
Lapparman <68941091+Lapparman@users.noreply.github.com>
2023-04-18 09:22:36 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export let options = {
+  stages: [
+    { duration: '10s', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '10s', target: 1000 },
+    { duration: '2m', target: 1000 },
+    { duration: '10s', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '10s', target: 1000 },
+    { duration: '2m', target: 1000 },
+    { duration: '10s', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '10s', target: 0 },
+
+  ],
+};
+
+export default function () {
+  http.get('https://tweet-app--performance-reyhan-aijk54j36a-et.a.run.app'); // Ganti dengan cloudrun URL
+  sleep(1);
+}
